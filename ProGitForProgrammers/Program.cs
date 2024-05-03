18dcd1a7a3f30acc4da986cd3ddd228e2ed30803
Isaac Cheng <42923555+isaaccwb@users.noreply.github.com>
2024-05-03 12:51:12 +0800

    Add in VS code

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -3,4 +3,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World! x 2");
 Console.WriteLine("Hello, World! x 3");
 Console.WriteLine("Hello, World! x 4");
-Console.WriteLine("Hello, World! x 12");      
+Console.WriteLine("Hello, World! x 12"); 
+Console.WriteLine("Hello, World! x 14");          
