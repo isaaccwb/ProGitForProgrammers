2edef2a39e8fe0e4e6979bdbb466064aa36d0251
Isaac Cheng <42923555+isaaccwb@users.noreply.github.com>
2024-05-03 13:08:52 +0800

    rebase

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -3,5 +3,9 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World! x 2");
 Console.WriteLine("Hello, World! x 3");
 Console.WriteLine("Hello, World! x 4");
+<<<<<<< HEAD
 Console.WriteLine("Hello, World! x 12"); 
 Console.WriteLine("Hello, World! x 14");          
+=======
+Console.WriteLine("Hello, World! x 13");      
+>>>>>>> 13b0ea8 (add in commandline)
